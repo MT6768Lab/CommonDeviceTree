d7a0f3a274211a2d05058502cffe4c7ebc2412b9
Jeferson <jroliveira.oliveira301@gmail.com>
2024-08-05 18:12:22 +0000

    mt6768-common: Dont dexpreopt prebuilts
    Change-Id: I443f227bfffc7edec5eb1b06bf7ccdb41da7577c
    Signed-off-by: Orkun Ergün <orkunergun68@gmail.com>

--- mt6768.mk
+++ mt6768.mk
@@ -124,6 +124,9 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# Dex
+DONT_DEXPREOPT_PREBUILTS := true
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
