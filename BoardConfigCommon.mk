51b5fcc320db330f1e463a3f6a1c2fc57cba4863
angelomds42 <angelomds42@gmail.com>
2026-06-10 11:35:28 +0000

    mt6768-common: cmdline: Disable kernel MTE
    * It is recommended by Google to keep it disabled in production builds for significant performance improvement.
    
    ref: https://source.android.com/docs/security/test/memory-safety/mte-configuration#kernel-s
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>
    Signed-off-by: fukiame <fukiame@proton.me>
    Signed-off-by: Orkun Ergün <104510596+orkunergun@users.noreply.github.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -57,6 +57,7 @@ BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_CMDLINE += noirqdebug
 BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem,nosocket
 BOARD_KERNEL_CMDLINE += cgroup_disable=pressure
+BOARD_KERNEL_CMDLINE += kasan=off
 
 # Display
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x2000U
